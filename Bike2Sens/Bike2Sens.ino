d00ffcab19e394f3dfbd592d26092b108bbc046d
Eric Leask <16efl@queensu.ca>
2018-07-27 22:59:05 -0400

    Reverse and Forward Completed
    Arduino will now properly send data for moving forward or backward

--- Bike2Sens/Bike2Sens.ino
+++ Bike2Sens/Bike2Sens.ino
@@ -1,13 +1,13 @@
-const int LEFT = A0;
-const int INP1 = A1;
-const int INP2 = A5;
-const int RIGHT = A2;
-const int SPECIAL = A3;
-const int DRIFT = A4;
+#define LEFT A0
+#define INP1 2
+#define INP2 3
+#define RIGHT A2
+#define SPECIAL A3
+#define DRIFT A4
 
-long dif[3] = {1, 1, 1};
-long t[3];
-byte v[3];
+long dif[2] = {1, 1};
+long t[3]; //time
+byte v[3];//Val of Input
 int i = 0;
 
 int delayTime = 500;
@@ -28,25 +28,19 @@ void setup() {
   pinMode(SPECIAL,  INPUT);
   pinMode(DRIFT,    INPUT);
 
-  digitalWrite(A0, HIGH);
-  digitalWrite(A1, HIGH);
-  digitalWrite(A2, HIGH);
-  digitalWrite(A3, HIGH);
-  digitalWrite(A4, HIGH);
-  digitalWrite(A5, HIGH);
+  digitalWrite(LEFT, HIGH);
+  digitalWrite(RIGHT, HIGH);
+  digitalWrite(SPECIAL, HIGH);
+  digitalWrite(DRIFT, HIGH);
 
-  attachInterrupt(digitalPinToInterrupt(INP1), upd81, RISING);
-  attachInterrupt(digitalPinToInterrupt(INP2), upd82, RISING);
+  attachInterrupt(0, upd81, RISING);
+  attachInterrupt(1, upd82, RISING);
 }
 
 void loop() {
-  if (!digitalRead(INP1) ) { //THIS MAY BREAK CHANGE TO analogRead
-    currTime = millis();
-  }
-
   if ((currTime + delayTime) > millis() && millis() > (delayTime + 50)) {
     checkIfFirstInput();
-    printVal[0] = true;
+    output();
   }
   if (digitalRead(LEFT) == LOW) {
     checkIfFirstInput();
@@ -82,7 +76,7 @@ void loop() {
       if (printVal[4] == true) {
         Serial.print("D:");
       }
-      if(printVal[5} == true){
+      if (printVal[5] == true) {
         Serial.print("B:");
       }
       Serial.println("N");
@@ -93,44 +87,45 @@ void loop() {
       printVal[i] = false;
     //on interrupt
   }
+}
+//------------Interrupts----------------------------------
+void upd81() {
+  if (v[2] != INP1) { //If most recent input is not itself
+    t[0] = t[1];//Update times
+    t[1] = t[2];
+    t[2] = millis(); //newest time in highest location
+    currTime = millis();
+    dif[1] = t[2] - t[1];//Calculate the new difference
+    dif[2] = t[3] - t[2];
 
-  void upd81() {
-    if (v[2] != INP1) {
-      t[0] = t[1];//Rotating times
-      t[1] = t[2];
-      t[2] = millis(); //newest time
-
-      dif[1] = t[2] - t[1];
-      dif[2] = t[3] - t[2];
-
-      v[0] = v[1];
-      v[1] = v[2];
-      v[2] = INP1;
-    }
+    v[0] = v[1];
+    v[1] = v[2];
+    v[2] = INP1;
   }
-  void upd82() {
-    if (v[2] != INP2) {
-      t[0] = t[1];
-      t[1] = t[2];
-      t[2] = millis();
-      dif[1] = t[2] - t[1];
-      dif[2] = t[3] - t[2];
-      v[0] = v[1];
-      v[1] = v[2];
-      v[2] = INP2;
-    }
+}
+void upd82() {
+  if (v[2] != INP2) {
+    t[0] = t[1];
+    t[1] = t[2];
+    t[2] = millis();
+    dif[1] = t[2] - t[1];
+    dif[2] = t[3] - t[2];
+    v[0] = v[1];
+    v[1] = v[2];
+    v[2] = INP2;
   }
-  void output{
-    
-    if (dif[1] < dif[2]) {//forward
-    printVal[0]=true;
-    }
-    else if(dif[2] < dif[1]){
-    printVal[5]=true;  
-    }
-if(dif[1]==dif[2]){
-  println("dif1=dif2");
-} 
+}
+void output() {
+
+  if (dif[1] < dif[2]) {//forward
+    printVal[0] = true;
+  }
+  else if (dif[2] < dif[1]) {
+    printVal[5] = true;
+  }
+  if (dif[1] == dif[2]) {
+    Serial.println("dif1=dif2");
+  }
 }
 void checkIfFirstInput() {
   if (firstInput == true) {
